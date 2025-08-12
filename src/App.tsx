91bc2d487c9f312ac984120a231cc2a3054a2dbb
juchanhwang <ghkdwncks96@gmail.com>
2025-08-12 22:23:23 +0900

    feat(tailwindcss): tailwindcss 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="a mx-auto max-w-7xl p-8 text-center">
+      <div className="flex justify-center">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img
+            src={viteLogo}
+            className="will-change-filter h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className="will-change-filter motion-safe:animate-spin-slow h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa]"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="my-8 text-5xl font-bold">Vite + React</h1>
+      <div className="p-8">
+        <button
+          className="rounded-lg border border-transparent bg-gray-100 px-4 py-2 text-base font-medium transition-colors hover:border-indigo-600 dark:bg-gray-800"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mt-4">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-gray-500 dark:text-gray-400">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
