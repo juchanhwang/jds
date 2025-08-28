8967c21f0150712a55144a86a29514428e4f903c
juchanhwang <ghkdwncks96@gmail.com>
2025-08-28 23:04:23 +0900

    chore(vite.config.ts): vite 설정 수정 - 빌드 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,28 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      // 라이브러리 진입점 파일
+      entry: path.resolve(__dirname, 'src/components/index.tsx'),
+      // 라이브러리 이름 (전역 변수로 사용될 때)
+      name: 'Jds',
+      // 최종 빌드 파일 이름
+      fileName: 'jds',
+    },
+    rollupOptions: {
+      // 라이브러리에 포함시키지 않을 외부 의존성
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        // 외부 의존성을 전역 변수로 사용하도록 설정
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss',
+        },
+      },
+    },
+  },
   test: {
     projects: [
       {
